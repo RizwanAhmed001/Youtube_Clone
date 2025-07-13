7cd1ffcb92a9910be0f781587d430d43759be5c1
Rizwan Ahmed <rizwanahmedrls96@gmail.com>
2025-07-13 12:49:31 +0530

    Comments are added on both backend and frontend

--- youtube-backend/models/channel.model.js
+++ youtube-backend/models/channel.model.js
@@ -1,37 +1,3 @@
-// const mongoose = require("mongoose")
-
-// const channelSchema = new mongoose.Schema({
-//     channelName: {
-//         type: String,
-//         required: true 
-//     },
-//     description: { 
-//         type: String 
-//     },
-//     channelPic: { 
-//         type: String 
-//     },
-//     channelBanner: { 
-//         type: String 
-//     },
-//     user: {
-//         type: mongoose.Schema.Types.ObjectId,
-//         ref: "user",
-//         required: true,
-//     },
-//     subscribers: { 
-//         type: Number, 
-//         default: 0 
-//     },
-//     video: [{ 
-//         type: mongoose.Schema.Types.ObjectId, 
-//         ref: "video" 
-//     }],
-// }, { timestamps: true });
-
-// module.exports = mongoose.model("channel", channelSchema);
-
-// Comments
 const mongoose = require("mongoose");
 
 // 🟢 Schema definition for a Channel document
